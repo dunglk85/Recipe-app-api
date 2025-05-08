0c56c05dab10f681bf8f9eaeae995b7b3466cae4
dunglk85 <dunglk85@gmail.com>
2025-05-09 04:07:17 +0700

    "ec2:DescribeAccountAttributes"

--- infra/setup/iam.tf
+++ infra/setup/iam.tf
@@ -353,7 +353,8 @@ data "aws_iam_policy_document" "elb" {
       "elasticloadbalancing:CreateTargetGroup",
       "elasticloadbalancing:AddTags",
       "elasticloadbalancing:DescribeTags",
-      "elasticloadbalancing:ModifyListener"
+      "elasticloadbalancing:ModifyListener",
+      "ec2:DescribeAccountAttributes"
     ]
     resources = ["*"]
   }
